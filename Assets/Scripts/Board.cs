f4188cfa25a6ecbcf971d90c2de0536b543dd67e
Melih Açıl <74834573+melihacil@users.noreply.github.com>
2024-01-29 15:54:09 +0300

    Adding tile scripts

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,27 +6,39 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-  public int width;
-  public int height;
-  public GameObject tilePrefab;
-  private TileBackGround[,] _allTiles;
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private TileBackGround[,] _allTiles;
+    public static Board instance;
 
-  private void Start()
-  {
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        instance = this;
+    }
+    private void Start()
+    {
     _allTiles = new TileBackGround[width, height];
     
     SetUp();
-  }
-
-  void SetUp()
-  {
-    for (var i = 0; i < width; i++)
-    {
-      for (var j = 0; j < height; j++)
-      {
-        var tempPosition = new Vector2(i, j);
-        var backGroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
-      }
     }
-  }
+
+    void SetUp()
+    {
+        for (var i = 0; i < width; i++)
+        {
+            for (var j = 0; j < height; j++)
+            {
+                var tempPosition = new Vector2(i, j);
+                var backGroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+
+                backGroundTile.GetComponent<Tile>().SetTilePosition(tempPosition);
+
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private Vector2 m_tilePosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetTilePosition(Vector2 tilePosition)
+    {
+        m_tilePosition = tilePosition;
+    }
+
+    // Checks the below 
+    private void CheckBottom()
+    {
+
+    }
+
+    private void MoveBottom()
+    {
+
+    }
+}
